79c26894aa0da6a03e9bee932bcc619b6762f587
mario-rutman <marrut@gmail.com>
2019-05-04 15:02:19 +0000

    incluindo a coluna tributo

--- tibble.R
+++ tibble.R
@@ -3,8 +3,9 @@
 library(tidyverse)
 
 raiz_cnpj <- rep(c(1:10), len = 106)
+tributo <- rep(c("ICMS", "ICMS", "FECP"), leng = 106)
 valor <- seq(2569, 9856, 69)  
 
-sdf <- tibble(raiz_cnpj, valor)
+sdf <- tibble(raiz_cnpj, tributo, valor)
 
 write_rds(sdf, "sdf.rds")
